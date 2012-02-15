913101c1a8fd5607bb3ef5ceaee805ea47ba2f47
Jonny Reeves <github@jonnyreeves.co.uk>
2012-02-15 22:30:54 +0000

    Lint'd.

--- src/jquery-Mustache.js
+++ src/jquery-Mustache.js
@@ -16,148 +16,151 @@
  * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
  * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-(function($) {
 
-	var _templateMap = {};
-	var _instance = null;
-	
-	var _options = {
-		// Should an error be thrown if an attempt is made to render a non-existant template.  If true, the operation 
-		// will fail silently.
-		warnOnMissingTemplates: false,
-		
-		// Should an error be thrown if an attempt is made to overwrite a template which has already been added.  
-		// If true the original template will be overwritten with the new value.
-		allowOverwrite: true
-	};
+/*global jQuery, window */
+(function ($, window) {
+	'use strict';
+
+	var templateMap = {},
+		instance = null,
+		options = {
+			// Should an error be thrown if an attempt is made to render a non-existant template.  If false, the  
+			// operation will fail silently.
+			warnOnMissingTemplates: false,
+
+			// Should an error be thrown if an attempt is made to overwrite a template which has already been added.  
+			// If true the original template will be overwritten with the new value.
+			allowOverwrite: true
+		};
+
+	function getMustache() {
+		// Lazily retrieve Mustache from the window global if it hasn't been defined by
+		// the User.
+		if (instance === null) {
+			instance = window.Mustache;
+			if (typeof (instance) === 'undefined') {
+				$.error("Failed to locate Mustache instance, are you sure it has been loaded?");
+			}
+		}
+		return instance;
+	}
+
+	/**
+	 * Returns true if the supplied templateName has been added.
+	 */
+	function has(templateName) {
+		return typeof (templateMap[templateName]) !== 'undefined';
+	}
 
 	/**
 	 * Registers a template so that it can be used by Mustache.
 	 * 
 	 * @param templateName		A name which uniquely identifies this template.
 	 * @param templateHtml		The HTML which makes us the template; this will be rendered by Mustache when render() 
-	 * 							is invoked.
+	 *							is invoked.
 	 * @throws					If options.allowOverwrite is false and the templateName has already been registered.
 	 */
 	function add(templateName, templateHtml) {
-		if (!_options.allowOverwrite && has(templateName)) {
+		if (!options.allowOverwrite && has(templateName)) {
 			$.error('TemplateName: ' + templateName + ' is already mapped.');
 			return;
 		}
-		_templateMap[templateName] = templateHtml;
-	};
-	
+		templateMap[templateName] = templateHtml;
+	}
+
 	/**
 	 * Removes a template, the contents of the removed Template will be returned.
 	 */
 	function remove(templateName) {
-		var result = _templateMap[templateName];
-		delete _templateMap[templateName];
+		var result = templateMap[templateName];
+		delete templateMap[templateName];
 		return result;
 	}
-	
+
 	/**
 	 * Removes all templates and tells Mustache to flush its cache.
 	 */
 	function clear() {
-		_templateMap = {};
+		templateMap = {};
 		getMustache().clearCache();
 	}
-	
-	/**
-	 * Returns true if the supplied templateName has been added.
-	 */
-	function has(templateName) {
-		return typeof(_templateMap[templateName]) !== 'undefined';
-	}
-	
-	/**
-	 * Loads the external Mustache templates located at the supplied URL and registers them for later use.  This method
-	 * returns a jQuery Promise and also support an `onComplete` callback.
-	 */
-	function load(url, onComplete) {
-		return $.Deferred(function(dfd) {
-			$.get(url)
-				.done(function(templates) {
-					$(templates).filter('script').each(function(i, el) {
-						add(el.id, $(el).html().trim());
-					});
-					
-					$.isFunction(onComplete) && onComplete();
-					dfd.resolve();
-				})
-				.fail(dfd.reject);
-		}).promise();
-	}
-	
+
 	/**
 	 * Renders a previously added Mustache template using the supplied templateData object.  Note if the supplied 
 	 * templateName doesn't exist an empty jQuery element will be returned.
 	 */
 	function render(templateName, templateData) {
 		if (!has(templateName)) {
-			if (!_options.warnOnMissingTemplates) {
+			if (options.warnOnMissingTemplates) {
 				$.error('No template registered for: ' + templateName);
 			}
 			return $(null);
 		}
-		return $(getMustache().to_html(_templateMap[templateName], templateData, _templateMap));
-	};
-	
+		return $(getMustache().to_html(templateMap[templateName], templateData, templateMap));
+	}
+
+	/**
+	 * Loads the external Mustache templates located at the supplied URL and registers them for later use.  This method
+	 * returns a jQuery Promise and also support an `onComplete` callback.
+	 */
+	function load(url, onComplete) {
+		return $.Deferred(function (dfd) {
+			$.get(url)
+				.done(function (templates) {
+					$(templates).filter('script').each(function (i, el) {
+						add(el.id, $(el).html().trim());
+					});
+
+					if ($.isFunction(onComplete)) {
+						onComplete();
+					}
+					dfd.resolve();
+				})
+				.fail(dfd.reject);
+		}).promise();
+	}
+
 	/**
 	 * Returns an Array of templateNames which have been registered and can be retrieved via $.Mustache.render() or 
 	 * $(element).mustache().
 	 */
 	function templates() {
-		return $.map(_templateMap, function(value, key) { 
+		return $.map(templateMap, function (value, key) {
 			return key;
 		});
-	};
-	
-	function getMustache() {
-		// Lazily retrieve Mustache from the window global if it hasn't been defined by
-		// the User.
-		if (_instance === null) {
-			_instance = window.Mustache;
-			if (typeof(_instance) === 'undefined') {
-				$.error("Failed to locate Mustache instance, are you sure it has been loaded?");
-			}
-		}
-		return _instance;
 	}
-	
+
 	// Expose the public methods on jQuery.Mustache
 	$.Mustache = {
-		options: _options,
+		options: options,
 		load: load,
 		add: add,
+		remove: remove,
+		clear: clear,
 		render: render,
 		templates: templates,
-		instance: _instance
-	};	
-	
+		instance: instance
+	};
+
 	/**
-	 * Render the template HTML mapped to the supplied templateName using the 
-	 * supplied templateData object.  Note the default action will replace the 
-	 * contents of the selected jQuery element.
+	 * Render the template HTML mapped to the supplied templateName using the  supplied templateData object.   
+	 * Note the default action will replace the contents of the selected jQuery element.
 	 * 
-	 * @param templateName		The name of the Mustache template you wish to render,
-	 *							Note that the Template must have been previously loaded
-	 *							and / or added.
-	 * @param templateData		JavaScript object which will be used to render the 
-	 * 							Mustache template.
-	 * @param options.method	jQuery method to use when rendering, defaults to 'html' which
-	 * 							replaces the contents of the current selector.
+	 * @param templateName		The name of the Mustache template you wish to render, Note that the Template must have 
+	 *							been previously loaded and / or added.
+	 * @param templateData		JavaScript object which will be used to render the Mustache template.
+	 * @param options.method	jQuery method to use when rendering, defaults to 'html' which replaces the contents of 
+	 *							the current selector.
 	 */
-	$.fn.mustache = function(templateName, templateData, options) {
+	$.fn.mustache = function (templateName, templateData, options) {
 		var settings = $.extend({
 			method:	'html'
 		}, options);
-		
-		return this.each(function() { 
+
+		return this.each(function () {
 			// Attach the contents to the current selector using the supplied method.
 			$(this)[settings.method](render(templateName, templateData));
 		});
 	};
-	
-})(jQuery);
+
+}(jQuery, window));
